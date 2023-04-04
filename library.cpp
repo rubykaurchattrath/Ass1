59bbe5bd34bf7518d220532904a3581a228ddb63
Ruby Kaur <rubykaur@uw.edu>
2023-04-04 11:03:40 -0700

    Version 1.0

--- library.cpp
+++ library.cpp
@@ -12,7 +12,7 @@
 // constructor with default name
 // Class::Constructor
 Library::Library(const string &Name) {
-  // Was Libs in main.cpp the library name
+  // Was Libs in main.cpp the library name?????
   Libs = Name;
 }
 
@@ -25,14 +25,12 @@ Library::~Library() {
 
 // true if book found in library
 bool Library::isInLibrary(const string &BookName) const {
-  // Where is BookName found?
   for (int i = 0; i < MAX; i++) {
     if (listOfBooks[i] == BookName) {
       return true;
     }
   }
 }
-
 
 // return the valid index if book is in library or -1 if book is not in library
 // using numberOfBooks because array may be partially filled
@@ -43,7 +41,8 @@ int findBook(const string& name) const {
 
   // If bag is empty, numberOfBooks is zero, so loop is skipped
   while (!isFound && (searchIndex < numberOfBooks)) {
-    isFound = (items[searchIndex] == target); 
+    // what is name though in this case?
+    isFound = (listOfBooks[searchIndex] == name); 
     if (isFound) {
       result = searchIndex;
     } else {
@@ -55,21 +54,33 @@ int findBook(const string& name) const {
 
 // add a new book
 // return true if successful, false if
-// book already in library
+// book already in library and/or if library is full
 bool Library::addBook(const string &BookName) {
-
+  if (!Libs::isInLibrary(BookName) && (numberOfBooks < MAX-1)) {
+    listOfBooks[numberOfBooks + 1] = BookName;
+    return true;
+  } else {
+    return false;
+  }
 }
 
 // remove a book
 // return true if successfully removed
 // false if book not in library
 bool Library::removeBook(const string &BookName) {
-  return false;
+  int locatedBook = findBook(BookName);
+  // can I even use isEmpty??
+  bool canRemoveBook = !isEmpty() && (locatedBook > -1);
+  if (canRemoveBook) {
+    numberOfBooks--;
+    listOfBooks[locatedBook] = listOfBooks[numberOfBooks];
+  }
+  return canRemoveBook;;
 }
 
 // list all books
 void Library::listAllBooks() const {
-  for (int i = numberOfBooks - 1; i >= 0; i--) {
+  for (int i = numberOfBooks - 1; i > -1; i--) {
     cout << listOfBooks[i];
   }
 }
